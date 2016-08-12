a63ee5c3e60db419c4da63c5c392546010a64393
Jesper Lindström <raxezdev@gmail.com>
2016-08-12 15:13:25 +0200

    Gradient view

---
+++ Chalmers Card/Chalmers Card/Views/GradientHeaderView.swift
@@ -0,0 +1,33 @@
+import UIKit
+
+class GradientHeaderView : UIView {
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        
+        setupView()
+    }
+    
+    override class func layerClass() -> AnyClass {
+        return CAGradientLayer.self
+    }
+    
+    private func setupView() {
+        autoresizingMask = [.FlexibleWidth, .FlexibleHeight]
+        
+        guard let gradient = self.layer as? CAGradientLayer else {
+            return;
+        }
+        
+        // Set color stops
+        gradient.colors = [
+            UIColor.colorWithHex(0x57E2CB).CGColor,
+            UIColor.colorWithHex(0x4492C6).CGColor
+        ]
+        
+        // Diagonal direction
+        gradient.startPoint = CGPointMake(0, 1)
+        gradient.endPoint = CGPointMake(1, 0)
+        
+        gradient.frame = bounds
+    }
+}
