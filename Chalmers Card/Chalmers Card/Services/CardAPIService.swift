4dd490919a6d111dbfbc069e310be44168cb46ad
Jesper Lindström <raxezdev@gmail.com>
2016-08-17 19:03:39 +0200

    Updated to use own API, before deploying

--- Chalmers Card/Chalmers Card/Services/CardAPIService.swift
+++ Chalmers Card/Chalmers Card/Services/CardAPIService.swift
@@ -3,26 +3,13 @@ import Alamofire
 
 class APIService {
     func getCardAmount(number: Int, callback: Int? -> Void) {
-        Alamofire.request(.GET, Config.apiUrl, parameters: ["card": number]).responseJSON { response in
-            if let json = response.result.value {
-                let success = json["successful"] as? Int
-                let amount = json["balance"] as? String
-                
-                guard let _ = success where success == 1,
-                    var cardAmount = amount else {
-                    return callback(nil)
-                }
-                
-                // Clean up integer
-                cardAmount = cardAmount.stringByReplacingOccurrencesOfString(" ", withString: "")
-                cardAmount = cardAmount.stringByReplacingOccurrencesOfString(",", withString: ".")
-                
-                guard let cardAmountFloat = Float(cardAmount) else {
-                    return callback(nil)
-                }
-                
-                callback(Int(cardAmountFloat))
+        print(Config.apiUrl + String(number))
+        Alamofire.request(.GET, Config.apiUrl + String(number)).responseJSON { response in
+            if let json = response.result.value, let amount = json["amount"] as? Int {
+                return callback(amount)
             }
+            
+            callback(nil)
         }
     }
 }
